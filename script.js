487ca7fb18b52318e5bf6044c9ea5ae7f403dcdf
d0n4to <145191374+d0n4to@users.noreply.github.com>
2024-04-16 23:23:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 // Navbar link
 let getAbout = document.getElementById("getAbout");
 let getResume = document.getElementById("getResume");
+let getProject = document.getElementById("getProject");
 let getContact = document.getElementById("getContact");
 
 // sezioni
 let about = document.getElementById("about");
 let resume = document.getElementById("resume");
+let project = document.getElementById("project");
 let contact = document.getElementById("contact");
 
 function removeClass() {
     // link
     getAbout.classList.remove('selected');
     getResume.classList.remove('selected');
+    getProject.classList.remove('selected');
     getContact.classList.remove('selected');
     // sezioni
     about.classList.remove('view');
     resume.classList.remove('view');
+    project.classList.remove('view');
     contact.classList.remove('view');
 }
 
@@ -43,4 +47,14 @@ getContact.addEventListener('click', function (e) {
         contact.classList.add('view');
         getContact.classList.add('selected');
     }
-})
+});
+
+getProject.addEventListener('click', function (e) {
+    if (window.innerWidth > 1040) {
+        e.preventDefault();
+        removeClass();
+        project.classList.add('view');
+        getProject.classList.add('selected');
+    }
+});
+
