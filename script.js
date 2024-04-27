6a38bdfbdc10b3a0c9d61373b4d9aaa7850959c8
D0n4to <145191374+d0n4to@users.noreply.github.com>
2024-04-27 08:16:24 +0200

    Update

--- script.js
+++ script.js
@@ -1,60 +1,71 @@
 // Navbar link
+// Seleziona i link della navbar tramite le loro ID
 let getAbout = document.getElementById("getAbout");
 let getResume = document.getElementById("getResume");
 let getProject = document.getElementById("getProject");
 let getContact = document.getElementById("getContact");
 
-// sezioni
+// Sezioni
+// Seleziona le sezioni corrispondenti alle ID
 let about = document.getElementById("about");
 let resume = document.getElementById("resume");
 let project = document.getElementById("project");
-let contact = document.getElementById("contact"); 
+let contact = document.getElementById("contact");
 
+// Funzione per rimuovere le classi 'selected' e 'view'
 function removeClass() {
-    // link
+    // Rimuove la classe 'selected' dai link della navbar
     getAbout.classList.remove('selected');
-    getResume.classList.remove('selected'); 
+    getResume.classList.remove('selected');
     getProject.classList.remove('selected');
     getContact.classList.remove('selected');
-    // sezioni
+    // Rimuove la classe 'view' dalle sezioni
     about.classList.remove('view');
     resume.classList.remove('view');
     project.classList.remove('view');
     contact.classList.remove('view');
 }
 
+// Aggiunge un evento di ascolto per il click su 'About'
 getAbout.addEventListener('click', function (e) {
+    // Controlla se la larghezza della finestra è maggiore di 1040 pixel
     if (window.innerWidth > 1040) {
-        e.preventDefault();
-        removeClass();
-        about.classList.add('view');
-        getAbout.classList.add('selected');
+        e.preventDefault(); // Previeni il comportamento predefinito del link
+        removeClass(); // Rimuove tutte le classi 'selected' e 'view'
+        about.classList.add('view'); // Aggiunge la classe 'view' alla sezione 'about'
+        getAbout.classList.add('selected'); // Aggiunge la classe 'selected' al link 'About'
     }
-
 });
+
+// Aggiunge un evento di ascolto per il click su 'Resume'
 getResume.addEventListener('click', function (e) {
+    // Controlla se la larghezza della finestra è maggiore di 1040 pixel
     if (window.innerWidth > 1040) {
-        e.preventDefault();
-        removeClass();
-        resume.classList.add('view');
-        getResume.classList.add('selected');
+        e.preventDefault(); // Previeni il comportamento predefinito del link
+        removeClass(); // Rimuove tutte le classi 'selected' e 'view'
+        resume.classList.add('view'); // Aggiunge la classe 'view' alla sezione 'resume'
+        getResume.classList.add('selected'); // Aggiunge la classe 'selected' al link 'Resume'
     }
-})
+});
+
+// Aggiunge un evento di ascolto per il click su 'Contact'
 getContact.addEventListener('click', function (e) {
+    // Controlla se la larghezza della finestra è maggiore di 1040 pixel
     if (window.innerWidth > 1040) {
-        e.preventDefault();
-        removeClass();
-        contact.classList.add('view');
-        getContact.classList.add('selected');
+        e.preventDefault(); // Previeni il comportamento predefinito del link
+        removeClass(); // Rimuove tutte le classi 'selected' e 'view'
+        contact.classList.add('view'); // Aggiunge la classe 'view' alla sezione 'contact'
+        getContact.classList.add('selected'); // Aggiunge la classe 'selected' al link 'Contact'
     }
 });
 
+// Aggiunge un evento di ascolto per il click su 'Project'
 getProject.addEventListener('click', function (e) {
+    // Controlla se la larghezza della finestra è maggiore di 1040 pixel
     if (window.innerWidth > 1040) {
-        e.preventDefault();
-        removeClass();
-        project.classList.add('view');
-        getProject.classList.add('selected');
+        e.preventDefault(); // Previeni il comportamento predefinito del link
+        removeClass(); // Rimuove tutte le classi 'selected' e 'view'
+        project.classList.add('view'); // Aggiunge la classe 'view' alla sezione 'project'
+        getProject.classList.add('selected'); // Aggiunge la classe 'selected' al link 'Project'
     }
 });
-
